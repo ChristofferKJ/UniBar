af1bcf9637210a0bf3d82627111a64597ddd33be
Hindkaer <hindkaer_msn@hotmail.com>
2019-11-21 11:08:07 +0100

    linked up the sign in components

--- Unibar2/app/src/main/java/com/group25/unibar/activities/SignInActivity.java
+++ Unibar2/app/src/main/java/com/group25/unibar/activities/SignInActivity.java
@@ -1,16 +1,51 @@
 package com.group25.unibar.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.group25.unibar.R;
 
-public class SignInActivity extends Activity {
+public class SignInActivity extends Activity implements View.OnClickListener {
+
+    EditText email;
+    EditText password;
+    Button sign_in;
+    TextView sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        email = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
+        sign_in = findViewById(R.id.buttonSignIn);
+        sign_up = findViewById(R.id.textViewSignUp);
+
+
+        sign_in.setOnClickListener(this);
+        sign_up.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.buttonSignIn:
+                Log.d("SignInActivity", "Sign In Pressed");
+                break;
+
+            case R.id.textViewSignUp:
+                Log.d("SignInActivity", "Sign Up Pressed");
+                break;
+
+        }
     }
 }
---
+++ Unibar2/app/src/main/java/com/group25/unibar/activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.group25.unibar.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.group25.unibar.R;
+
+public class SignUpActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
